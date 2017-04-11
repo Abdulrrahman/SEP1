136309e84b27f10aff6a83b73413cfe829eca378
MR.Joukhadar <A.joukhadar@hotmail.com>
2017-04-11 14:10:52 +0300

    resize images

--- S.E.P/www/js/controllers.js
+++ S.E.P/www/js/controllers.js
@@ -266,7 +266,7 @@ angular.module('SimpleRESTIonic.controllers', [])
         mytimeout = $timeout;
         mv.starttimeer = $timeout;
         mv.start = start;
-        mv.q0 = 1;
+        mv.q0 = 16;
         $scope.choice = [false, false, false, false];
         mv.result = [0, 0, 0, 0];
         mv.lang = [{ tr: true }, { en: false }];
